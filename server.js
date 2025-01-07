98a5b1a6407c701d158506062aa9232b4946b7a6
elifb <elifbaksi@gmail.com>
2025-01-07 16:10:31 -0500

    Fix MongoDB connection and remove deprecated options

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Meal Planner API!');
+});
 app.use('/api/recipes', require('./routes/recipes'));
 app.use('/api/meal-plans', require('./routes/mealPlans'));
 app.use('/api/shopping-lists', require('./routes/shoppingLists'));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
